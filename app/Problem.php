b24f5f7113bed6ec49f8f4f7d84a37efab44be48
Chip Bell <cbell@pbs.org>
2016-08-01 15:15:27 -0400

    Connecting problems back to tags

--- app/Problem.php
+++ app/Problem.php
@@ -12,4 +12,9 @@ class Problem extends Model
     {
         return $this->belongsTo('App\CompetitionProblemSet');
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
 }
