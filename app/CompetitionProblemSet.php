7bb9a00072d1c67f44639d42f36d057f1133f49e
Chip Bell <cbell@pbs.org>
2016-08-01 14:21:17 -0400

    Moving everything to an uploads directory first

--- app/CompetitionProblemSet.php
+++ app/CompetitionProblemSet.php
@@ -3,13 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpFoundation\File\File;
 
 class CompetitionProblemSet extends Model
 {
     protected $fillable = ['name', 'filename', 'file'];
 
-    public function setFileAttribute(\SplFileInfo $value)
+    public function setFileAttribute(File $value)
     {
-        $this->filename = $value->getRealPath();
+        $new_home = $value->move(storage_path() . '/uploads', 10000 * microtime(true) . '.pdf');
+        $this->filename = $new_home->getRealPath();
     }
 }
