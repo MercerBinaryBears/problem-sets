0b9261fe9c28d4992fc1fe171bf5103c6f24adc2
Chip Bell <cbell@pbs.org>
2016-08-01 20:44:57 -0400

    pulling in the administrator package

--- config/app.php
+++ config/app.php
@@ -156,6 +156,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Some extra providers
+        'Frozennode\Administrator\AdministratorServiceProvider',
     ],
 
     /*
