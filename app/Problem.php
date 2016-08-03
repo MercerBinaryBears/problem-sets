2c60263e20f26f6302ad534319a6ca442c77c460
Chip Bell <cbell@pbs.org>
2016-08-03 12:55:39 -0400

    Making the problem automatically update pdfs on the fly

--- app/Problem.php
+++ app/Problem.php
@@ -8,6 +8,15 @@ class Problem extends Model
 {
     protected $fillable = ['name', 'sample_input', 'sample_output', 'judge_input', 'judge_output', 'start_page', 'end_page', 'competition_problem_set_id'];
 
+    public static function boot() {
+        $do_split = function($problemSet) {
+            \Artisan::call('join:problem', ['problem_id' => $problemSet->id]);
+        };
+
+        Problem::created($do_split);
+        Problem::updated($do_split);
+    }
+
     public function competitionProblemSet()
     {
         return $this->belongsTo('App\CompetitionProblemSet');
