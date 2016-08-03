66dd8a5a76d4b9a7b46dcc543a37f3e4f787caa1
Chip Bell <cbell@pbs.org>
2016-08-02 22:04:28 -0400

    Adding automatic page splitting for a competition problem set

--- app/CompetitionProblemSet.php
+++ app/CompetitionProblemSet.php
@@ -19,4 +19,18 @@ class CompetitionProblemSet extends Model
     {
         return $this->hasMany('App\Problem');
     }
+
+    public function getFullPathAttribute()
+    {
+        return storage_path('uploads') . '/' . $this->filename;
+    }
+
+    public static function boot()
+    {
+        CompetitionProblemSet::created(function($problemSet) {
+            $output_directory = storage_path('split_pages') . '/';
+            $output_files = \App::make('App\Services\PdfService')->split($problemSet->fullPath, $output_directory);
+            \Log::info($problemSet->name . ' split ' . var_export($output_files, true));
+        });
+    }
 }
