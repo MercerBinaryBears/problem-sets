24eb7bfcf3b41d81dbfc3d25b4a8c27eac99b615
Chip Bell <cbell@pbs.org>
2016-08-06 15:42:38 -0400

    Font stack

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         @section('styles')
+        <link href='http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" type="text/css" href="{{ elixir('css/app.css') }}" />
         @show
     </head>
