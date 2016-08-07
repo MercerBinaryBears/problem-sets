484b8f3fb1653bdd2a94398a03b604891e3de7d6
Chip Bell <cbell@pbs.org>
2016-08-07 11:57:08 -0400

    Moving to a subview

---
+++ resources/views/partials/problem_search_form.blade.php
@@ -0,0 +1,11 @@
+<form class="grid" action="/problems">
+    <label class="grid-25" for="name">Name</label>
+    <input id="name" class="grid-75" name="name" type="text" value="{{ $searched_name }}" />
+    <label class="grid-25" for="tags">Tags</label>
+    <select name="tags" class="grid-75" multiple="multiple" data-chosen="true">
+    @foreach($tags as $tag)
+        <option value="{{ $tag->id }}" {{ in_array($tag->id, $searched_tags) ? "selected=selected" : null }} >{{ $tag->name }}</option>
+    @endforeach
+    </select>
+    <button class="grid-100">Search</button>
+</form>
--- resources/views/problems/all.blade.php
+++ resources/views/problems/all.blade.php
@@ -6,17 +6,8 @@
 @endsection
 
 @section('content')
-        <form class="grid" action="/problems">
-            <label class="grid-25" for="name">Name</label>
-            <input id="name" class="grid-75" name="name" type="text" value="{{ $searched_name }}" />
-            <label class="grid-25" for="tags">Tags</label>
-            <select name="tags" class="grid-75" multiple="multiple" data-chosen="true">
-            @foreach($tags as $tag)
-                <option value="{{ $tag->id }}" {{ in_array($tag->id, $searched_tags) ? "selected=selected" : null }} >{{ $tag->name }}</option>
-            @endforeach
-            </select>
-            <button class="grid-100">Search</button>
-        </form>
+        @include('partials.problem_search_form')
+
         <ul class="search-results">
         @foreach($search_results as $problem)
             <li>
