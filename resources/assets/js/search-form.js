e2ca9487bfd44b58762ba43d2fdc1274afae9e48
Chip Bell <cbell@pbs.org>
2016-08-07 13:03:42 -0400

    Making sure to bind the problem count properly

--- resources/assets/js/search-form.js
+++ resources/assets/js/search-form.js
@@ -1,15 +1,22 @@
 $(function() {
   $('form[data-search-form]').on('submit', function() {
+    var $form = $(this);
+
     var query = {};
 
-    var $name = $('[name=name]');
+    var $name = $('[name=name]', $form);
     if($name.length > 0) {
       query.name = $name.val();
     }
 
-    var $tags = $('[name=tags]');
+    var $tags = $('[name=tags]', $form);
     if($tags.length > 0) {
       query.tags = $tags.val().join(',');
+    }
+
+    var $problemCount = $('[name=problem_count]', $form);
+    if($problemCount.length > 0) {
+      query.problem_count = $problemCount.val();
     }
 
     var url = $(this).attr('action') + '?' + $.param(query);
