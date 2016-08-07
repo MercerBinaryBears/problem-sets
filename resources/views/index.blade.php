1001920b198137bd1257be892a7c2b7fd20cbef1
Chip Bell <cbell@pbs.org>
2016-08-07 12:20:40 -0400

    Wiring up tags properly

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,7 @@
     </div>
     <div class="grid-33">
         <h2>Search Problems</h2>
-        @include('partials.problem_search_form', ['searched_name' => '', 'tags' => []])
+        @include('partials.problem_search_form', ['searched_name' => '', 'tags' => \App\Tag::all(), 'searched_tags' => []])
     </div>
     <div class="grid-33">
         Hey
