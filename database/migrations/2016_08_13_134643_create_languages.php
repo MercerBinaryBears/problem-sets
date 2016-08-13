da6319f8c41c47b390adea4a8968c4bc063839a6
Chip Bell <chip.bell@spinen.com>
2016-08-13 09:48:13 -0400

    Adding a languages model and migration

---
+++ database/migrations/2016_08_13_134643_create_languages.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLanguages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('languages', function($table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('color');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('languages');
+    }
+}
